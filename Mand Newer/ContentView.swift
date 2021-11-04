2ecb7f8359993922025182119f7c0f1ca5ab110c
Denise Case <denisecase@gmail.com>
2021-11-04 09:12:14 -0500

    Add GitH

--- Mand Newer/ContentView.swift
+++ Mand Newer/ContentView.swift
@@ -275,10 +275,10 @@ struct ContentView: View {
     }
 }
 
-
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
