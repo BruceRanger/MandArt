50aaee3614f9272d85ce114cadfd00fc5ce5905f
Denise Case <denisecase@gmail.com>
2022-11-27 23:07:49 -0600

    20 hues

--- MandArt/PictureDefinition.swift
+++ MandArt/PictureDefinition.swift
@@ -23,12 +23,26 @@ class PictureDefinition: Codable, ObservableObject {
     var dFIterMinStart: Double =  10.0
     var nColorsStart: Int = 6
     var leftNumberStart: Int = 1
-    var hues: [Hue]? = [
+    var hues: [Hue] = [
         Hue(numberStart:1, rStart:0.0, gStart:255.0, bStart:0.0),
         Hue(numberStart:2, rStart:255.0, gStart:255.0, bStart:0.0),
         Hue(numberStart:3,rStart:255.0, gStart:0.0, bStart:0.0),
         Hue(numberStart:4,rStart:255.0, gStart:0.0, bStart:255.0),
         Hue(numberStart:5,rStart:0.0, gStart:0.0, bStart:255.0),
-        Hue(numberStart:6,rStart:0.0, gStart:255.0, bStart:255.0)
+        Hue(numberStart:6,rStart:0.0, gStart:255.0, bStart:255.0),
+        Hue(numberStart:7,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:8,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:9,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:10,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:11,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:12,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:13,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:14,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:15,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:16,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:17,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:18,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:19,rStart:0.0, gStart:0.0, bStart:0.0),
+        Hue(numberStart:20,rStart:0.0, gStart:0.0, bStart:0.0)
     ]
 }
