d0e323e42bc8e2230d85d5d5a509bd1444165f97
Denise Case <denisecase@gmail.com>
2023-02-05 13:58:17 -0600

    Swift format

--- MandArt/Hue.swift
+++ MandArt/Hue.swift
@@ -8,8 +8,6 @@
 import Foundation
 import SwiftUI
 
-
-
 /// Hue is a **sorted color** used in a MandArt image.
 /// It includes the user-specified sort order,
 /// the individual r, g, and b files, and a
@@ -19,58 +17,53 @@ import SwiftUI
 /// .
 class Hue: Codable, Identifiable, ObservableObject {
     var id = UUID()
-    var num: Int =  1
-    var r: Double =  0.0
-    var g: Double =  255.0
-    var b: Double =  0.0
-    var color : Color
+    var num: Int = 1
+    var r: Double = 0.0
+    var g: Double = 255.0
+    var b: Double = 0.0
+    var color: Color
 
     // default new entry will be white, with a num of zero
     init() {
-        self.num = 0
-        self.r = 255
-        self.g = 255
-        self.b = 255
-        self.color = Color(.sRGB,red: self.r/255,green: self.g/255,blue: self.b/255)
+        num = 0
+        r = 255
+        g = 255
+        b = 255
+        color = Color(.sRGB, red: r / 255, green: g / 255, blue: b / 255)
     }
 
     // initialize with a given num and rgb values
-    init(num:Int, r:Double,g:Double,b:Double) {
+    init(num: Int, r: Double, g: Double, b: Double) {
         self.num = num
         self.r = r
         self.g = g
         self.b = b
-        self.color = Color(.sRGB,red: self.r/255,green: self.g/255,blue: self.b/255)
+        color = Color(.sRGB, red: self.r / 255, green: self.g / 255, blue: self.b / 255)
     }
 
     func updateColorFromPicker(cgColor: CGColor) {
         if let arr = cgColor.components {
-            self.r = arr[0]
-            self.g = arr[1]
-            self.b = arr[2]
-        }
-        else{
-            self.r = 0
-            self.g = 0
-            self.b = 0
+            r = arr[0]
+            g = arr[1]
+            b = arr[2]
+        } else {
+            r = 0
+            g = 0
+            b = 0
         }
     }
-
 }
-
-
 
 extension Hue: Equatable {
-    static func ==(lhs: Hue, rhs: Hue) -> Bool {
+    static func == (lhs: Hue, rhs: Hue) -> Bool {
         lhs.id == rhs.id &&
-        lhs.num == rhs.num &&
-        lhs.r == rhs.r &&
-        lhs.g == rhs.g &&
-        lhs.b == rhs.b &&
-        lhs.color == rhs.color
+            lhs.num == rhs.num &&
+            lhs.r == rhs.r &&
+            lhs.g == rhs.g &&
+            lhs.b == rhs.b &&
+            lhs.color == rhs.color
     }
 }
-
 
 // ***********
 
@@ -79,12 +72,12 @@ extension Hue: Equatable {
 // http://brunowernimont.me/howtos/make-swiftui-color-codable
 //
 
-fileprivate extension Color {
-#if os(macOS)
-    typealias SystemColor = NSColor
-#else
-    typealias SystemColor = UIColor
-#endif
+private extension Color {
+    #if os(macOS)
+        typealias SystemColor = NSColor
+    #else
+        typealias SystemColor = UIColor
+    #endif
 
     var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
         var r: CGFloat = 0
@@ -92,16 +85,16 @@ fileprivate extension Color {
         var b: CGFloat = 0
         var a: CGFloat = 0
 
-#if os(macOS)
-        SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+        #if os(macOS)
+            SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
         // Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
-#else
-        guard SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else {
-            // Pay attention that the color should be convertible into RGB format
-            // Colors using hue, saturation and brightness won't work
-            return nil
-        }
-#endif
+        #else
+            guard SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else {
+                // Pay attention that the color should be convertible into RGB format
+                // Colors using hue, saturation and brightness won't work
+                return nil
+            }
+        #endif
 
         return (r, g, b, a)
     }
@@ -122,7 +115,7 @@ extension Color: Codable {
     }
 
     public func encode(to encoder: Encoder) throws {
-        guard let colorComponents = self.colorComponents else {
+        guard let colorComponents = colorComponents else {
             return
         }
 
@@ -133,5 +126,3 @@ extension Color: Codable {
         try container.encode(colorComponents.blue, forKey: .blue)
     }
 }
-
-
--- MandArt/PictureDefinition.swift
+++ MandArt/PictureDefinition.swift
@@ -13,39 +13,37 @@ import SwiftUI
 struct PictureDefinition: Codable, Identifiable {
     var id = UUID()
     var xC: Double = -0.74725
-    var yC: Double =  0.08442
-    var scale: Double =  2_880_000.0
-    var iMax: Double =  10_000.0
-    var rSqLimit: Double =  400.0
-    var imageWidth: Int = 1_200
-    var imageHeight: Int = 1_000
-    var nBlocks: Int =  60
-    var bE: Double =  5.0
-    var eE: Double =  15.0
-    var yY: Double =  0.0
-    var theta: Double =  0.0
+    var yC: Double = 0.08442
+    var scale: Double = 2_880_000.0
+    var iMax: Double = 10000.0
+    var rSqLimit: Double = 400.0
+    var imageWidth: Int = 1200
+    var imageHeight: Int = 1000
+    var nBlocks: Int = 60
+    var bE: Double = 5.0
+    var eE: Double = 15.0
+    var yY: Double = 0.0
+    var theta: Double = 0.0
     var nImage: Int = 0
     var dFIterMin: Double = 0.0
     var nColors: Int = 6
     var leftNumber: Int = 1
     var hues: [Hue] = [
-        Hue(num:1, r:0.0, g:255.0, b:0.0),
-        Hue(num:2, r:255.0, g:255.0, b:0.0),
-        Hue(num:3, r:255.0, g:0.0, b:0.0),
-        Hue(num:4, r:255.0, g:0.0, b:255.0),
-        Hue(num:5, r:0.0, g:0.0, b:255.0),
-        Hue(num:6, r:0.0, g:255.0, b:255.0),
+        Hue(num: 1, r: 0.0, g: 255.0, b: 0.0),
+        Hue(num: 2, r: 255.0, g: 255.0, b: 0.0),
+        Hue(num: 3, r: 255.0, g: 0.0, b: 0.0),
+        Hue(num: 4, r: 255.0, g: 0.0, b: 255.0),
+        Hue(num: 5, r: 0.0, g: 0.0, b: 255.0),
+        Hue(num: 6, r: 0.0, g: 255.0, b: 255.0),
     ]
     var huesEstimatedPrintPreview: [Hue] = []
     var huesOptimizedForPrinter: [Hue] = []
 
-
     /// Initialize with an array of Hues (sorted rgbs)
     /// - Parameter hues: an array of hues
-    init(hues:[Hue]){
+    init(hues: [Hue]) {
         self.hues = hues
     }
-
 
     /// Initialize by setting everything.
     /// - Parameters:
@@ -83,79 +81,76 @@ struct PictureDefinition: Codable, Identifiable {
         nColors: Int,
         leftNumber: Int,
         hues: [Hue]
-        ){
-            self.xC = xC
-            self.yC = yC
-            self.scale = scale
-            self.iMax = iMax
-            self.rSqLimit = rSqLimit
-            self.imageWidth = imageWidth
-            self.imageHeight = imageHeight
-            self.nBlocks = nBlocks
-            self.bE = bE
-            self.eE = eE
-            self.yY = yY
-            self.theta = theta
-            self.nImage = nImage
-            self.dFIterMin = dFIterMin
-            self.nColors = nColors
-            self.leftNumber = leftNumber
-            self.hues = hues
-            }
+    ) {
+        self.xC = xC
+        self.yC = yC
+        self.scale = scale
+        self.iMax = iMax
+        self.rSqLimit = rSqLimit
+        self.imageWidth = imageWidth
+        self.imageHeight = imageHeight
+        self.nBlocks = nBlocks
+        self.bE = bE
+        self.eE = eE
+        self.yY = yY
+        self.theta = theta
+        self.nImage = nImage
+        self.dFIterMin = dFIterMin
+        self.nColors = nColors
+        self.leftNumber = leftNumber
+        self.hues = hues
+    }
 
     private func getHueFromLookupResponse(response: String, sortOrder: Int) -> Hue {
         // response is in format "000-000-000" need to get r / g / b
-        let rStr: String = response[0..<3]
-        let gStr: String = response[4..<7]
-        let bStr: String = response[8..<11]
-        let red: Double = Double(rStr)!
-        let green: Double = Double(gStr)!
-        let blue: Double = Double(bStr)!
-        return Hue(num:sortOrder, r:red, g:green, b:blue)
+        let rStr: String = response[0 ..< 3]
+        let gStr: String = response[4 ..< 7]
+        let bStr: String = response[8 ..< 11]
+        let red = Double(rStr)!
+        let green = Double(gStr)!
+        let blue = Double(bStr)!
+        return Hue(num: sortOrder, r: red, g: green, b: blue)
     }
-
 
     /// Convert from a precise R G B double (0-255)
     ///  To a general bucketed value (0, 34, 68, 102, 136, 170, 204, 238, 255)
     /// - Parameter preciseValue: preciseValue a double from 0 to 255
     /// - Returns: a bucketed Double equal or less than the precise value
     fileprivate func getBucketColorDouble(preciseValue: Double) -> Double {
-        var bucketValue : Double = 0.0
+        var bucketValue = 0.0
         switch preciseValue {
-            case 0..<34:
-                bucketValue = 0
-            case 34..<68:
-                bucketValue = 34
-            case 68..<102:
-                bucketValue = 68
-            case 102..<136:
-                bucketValue = 102
-            case 136..<170:
-                bucketValue = 136
-            case 170..<204:
-                bucketValue = 170
-            case 204..<238:
-                bucketValue = 204
-            case 238..<255:
-                bucketValue = 238
-            case 255:
-                bucketValue = 255
-            default:
-                fatalError()
+        case 0 ..< 34:
+            bucketValue = 0
+        case 34 ..< 68:
+            bucketValue = 34
+        case 68 ..< 102:
+            bucketValue = 68
+        case 102 ..< 136:
+            bucketValue = 102
+        case 136 ..< 170:
+            bucketValue = 136
+        case 170 ..< 204:
+            bucketValue = 170
+        case 204 ..< 238:
+            bucketValue = 204
+        case 238 ..< 255:
+            bucketValue = 238
+        case 255:
+            bucketValue = 255
+        default:
+            fatalError()
         }
         return bucketValue
     }
 
     fileprivate func getLookupStringFromHue(hue: Hue) -> String {
-        let bucketR : Double = getBucketColorDouble(preciseValue:hue.r)
-        let bucketG : Double = getBucketColorDouble(preciseValue:hue.g)
-        let bucketB : Double = getBucketColorDouble(preciseValue:hue.b)
-        let strR:String = String(format: "%03d", Int(bucketR))
-        let strG:String = String(format: "%03d", Int(bucketG))
-        let strB:String = String(format: "%03d", Int(bucketB))
-        let lookupString:String = strR + "-"+strG + "-"+strB
+        let bucketR: Double = getBucketColorDouble(preciseValue: hue.r)
+        let bucketG: Double = getBucketColorDouble(preciseValue: hue.g)
+        let bucketB: Double = getBucketColorDouble(preciseValue: hue.b)
+        let strR = String(format: "%03d", Int(bucketR))
+        let strG = String(format: "%03d", Int(bucketG))
+        let strB = String(format: "%03d", Int(bucketB))
+        let lookupString: String = strR + "-" + strG + "-" + strB
         return lookupString
     }
-
-
 }
