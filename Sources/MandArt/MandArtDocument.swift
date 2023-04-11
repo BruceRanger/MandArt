8b6fc9820d75d25d7c29b11d4521f649e5f5b9bc
Denise Case <denisecase@gmail.com>
2023-04-11 10:49:08 -0500

    Add Description to png files with x and y

--- Sources/MandArt/MandArtDocument.swift
+++ Sources/MandArt/MandArtDocument.swift
@@ -107,11 +107,7 @@ final class MandArtDocument: ReferenceFileDocument, ObservableObject {
     }
   }
 
-  // Save the image to a file.
-  func saveMandArtImage() {
-    let winTitle = self.getCurrentWindowTitle()
-    let justname = winTitle.replacingOccurrences(of: ".mandart", with: "")
-    let imageFileName = justname + ".png"
+  func beforeSaveImage() {
     var data: Data
     do {
       data = try JSONEncoder().encode(self.picdef)
@@ -129,28 +125,118 @@ final class MandArtDocument: ReferenceFileDocument, ObservableObject {
       NSApplication.shared.terminate(nil)
       exit(99)
     }
-
     // trigger state change to force a current image
     self.picdef.imageHeight += 1
     self.picdef.imageHeight -= 1
+  }
 
-    let currImage = contextImageGlobal!
+  func getDefaultImageFileName() -> String {
+    let winTitle = self.getCurrentWindowTitle()
+    let justname = winTitle.replacingOccurrences(of: ".mandart", with: "")
+    let imageFileName = justname + ".png"
+    return imageFileName
+  }
+
+  func getImageComment() -> String {
+    let xString = String(picdef.xCenter)
+    let yString = String(picdef.yCenter)
+    let comment = "x is \(xString) and y is \(yString)"
+    return comment
+  }
+
+  func initSavePanel(fn: String) -> NSSavePanel {
     let savePanel = NSSavePanel()
     savePanel.title = "Choose Directory for MandArt image"
-    savePanel.nameFieldStringValue = imageFileName
+    savePanel.nameFieldStringValue = fn
     savePanel.canCreateDirectories = true
+    return savePanel
+  }
+
+  // requires Cocoa
+  // requires ImageIO
+  func setPNGDescription(imageURL: URL, description: String) throws {
+    // Get the image data
+    guard let imageData = try? Data(contentsOf: imageURL) else {
+      throw NSError(domain: "com.bhj.mandart", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to read image data"])
+    }
+
+    // Create a CGImageSource from the image data
+    guard let imageSource = CGImageSourceCreateWithData(imageData as CFData, nil) else {
+      throw NSError(domain: "com.bhj.mandart", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to create image source"])
+    }
+
+    // Create a CGImageDestination to write the image with metadata
+    guard let destination = CGImageDestinationCreateWithURL(imageURL as CFURL, kUTTypePNG, 1, nil) else {
+      throw NSError(domain: "com.bhj.mandart", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to create image destination"])
+    }
+
+    // Get the image properties dictionary
+    guard let properties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as? [CFString: Any] else {
+      throw NSError(domain: "com.bhj.mandart", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to get image properties"])
+    }
+
+    // Create a mutable copy of the properties dictionary
+    var mutableProperties = properties as [CFString: Any]
+
+    // Add the PNG dictionary with the description attribute
+    var pngProperties = [CFString: Any]()
+    pngProperties[kCGImagePropertyPNGDescription] = description
+    mutableProperties[kCGImagePropertyPNGDictionary] = pngProperties
+
+    // Add the image to the destination with metadata
+    CGImageDestinationAddImageFromSource(destination, imageSource, 0, mutableProperties as CFDictionary)
+
+    // Finalize the destination to write the image with metadata to disk
+    guard CGImageDestinationFinalize(destination) else {
+      throw NSError(domain: "com.bhj.mandart", code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to write image with metadata to disk"])
+    }
+  }
+
+
+  func saveMandArtImage() {
+    beforeSaveImage()
+    guard let cgImage = contextImageGlobal else {
+      print("Error: No context image available.")
+      return
+    }
+    let imageFileName: String = getDefaultImageFileName()
+    let comment: String = getImageComment()
+    let savePanel: NSSavePanel = initSavePanel(fn: imageFileName)
+
+    // Set the description attribute in the PNG metadata
+    let pngMetadata: [String: Any] = [
+      kCGImagePropertyPNGDescription as String: comment
+    ]
+
     savePanel.begin { result in
       if result == .OK, let url = savePanel.url {
-        let dest = CGImageDestinationCreateWithURL(url as CFURL, kUTTypeJPEG, 1, nil)
-        CGImageDestinationAddImage(dest!, currImage, nil)
-        if CGImageDestinationFinalize(dest!) {
-          print("Image saved successfully to \(url)")
-        } else {
-          print("Error saving image")
+        let imageData = cgImage.pngData()!
+        let ciImage = CIImage(data: imageData, options: [.properties: pngMetadata])
+        let context = CIContext(options: nil)
+
+        guard let pngData = context.pngRepresentation(of: ciImage!, format: .RGBA8, colorSpace: ciImage!.colorSpace!) else {
+          print("Error: Failed to generate PNG data.")
+          return
+        }
+
+        do {
+          try pngData.write(to: url, options: .atomic)
+          print("Saved image to: \(url)")
+          print("Description: \(comment)")
+          // Description is not saved.
+          // The following is needed to actually write the description
+          let imageURL = url
+          let description = comment
+          try self.setPNGDescription(imageURL: imageURL, description: description)
+
+
+        } catch let error {
+          print("Error saving image: \(error)")
         }
       }
     }
   }
+
 
   /**
    Create a snapshot of the current state of the document for serialization
@@ -339,4 +425,21 @@ extension String {
 @available(macOS 11.0, *)
 extension UTType {
   static let mandartDocType = UTType(importedAs: "org.bhj.mandart")
+}
+
+
+/** Extend CGImage to add pngData()
+ requires Cocoa
+ */
+@available(macOS, introduced: 10.13)
+extension CGImage {
+  func pngData() -> Data? {
+    let mutableData = CFDataCreateMutable(nil, 0)!
+    let dest = CGImageDestinationCreateWithData(mutableData, kUTTypePNG, 1, nil)!
+    CGImageDestinationAddImage(dest, self, nil)
+    if CGImageDestinationFinalize(dest) {
+      return mutableData as Data
+    }
+    return nil
+  }
 }
