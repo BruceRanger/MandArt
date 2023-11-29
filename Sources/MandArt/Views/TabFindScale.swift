9d9aebb5b388c26c6e3196b1d8062182e6cf4612
Denise Case <denisecase@gmail.com>
2023-11-29 13:09:07 -0600

    set scale by doc, by text, or zooms

--- Sources/MandArt/Views/TabFindScale.swift
+++ Sources/MandArt/Views/TabFindScale.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
+/// A view for adjusting and managing the scale of an image in the MandArt project.
+///
+/// This view provides a user interface for changing the image scale either through
+/// direct input, standard zooming (by a factor of 2), or custom zooming based on a multiplier.
 @available(macOS 12.0, *)
 struct TabFindScale: View {
   @ObservedObject var doc: MandArtDocument
@@ -10,6 +14,11 @@ struct TabFindScale: View {
   @State private var didChange: Bool = false
   @State private var scaleString: String = ""
 
+  /// Initializes the view with a document and a binding to determine if a full calculation is required.
+  ///
+  /// - Parameters:
+  ///   - doc: The document containing the image and scale data.
+  ///   - requiresFullCalc: A binding to a Boolean indicating whether a full recalculation is required.
   init(doc: MandArtDocument, requiresFullCalc: Binding<Bool>) {
     _doc = ObservedObject(initialValue: doc)
     _requiresFullCalc = requiresFullCalc
@@ -17,34 +26,35 @@ struct TabFindScale: View {
     _scaleString = State(initialValue: String(format: "%.0f", doc.picdef.scale))
   }
 
+  /// Updates the scale value and synchronizes it across the document, the scale state, and the text field.
+  ///
+  /// - Parameter newScale: The new scale value to be set.
+  func updateScale(newScale: Double) {
+    scale = newScale.rounded()
+    doc.picdef.scale = scale
+    scaleString = String(format: "%.0f", scale)
+    didChange = !didChange // Force redraw if needed
+    requiresFullCalc = true
+  }
+
+  /// Increases the scale by a factor of 2.
   func zoomIn() {
-    scale *= 2.0
-    scale = scale.rounded() // Round to the nearest integer
-    doc.picdef.scale = scale
-    didChange = !didChange // force redraw
+    updateScale(newScale: scale * 2.0)
   }
 
+  /// Decreases the scale by a factor of 2.
   func zoomOut() {
-    scale /= 2.0
-    scale = scale.rounded() // Round to the nearest integer
-    doc.picdef.scale = scale
-    didChange = !didChange // force redraw
+    updateScale(newScale: scale / 2.0)
   }
 
+  /// Increases the scale by the custom scale multiplier.
   func zoomInCustom() {
-    scale *= scaleMultiplier
-    scale = scale.rounded() // Round to the nearest integer
-    doc.picdef.scale = scale
-    requiresFullCalc = true
-    didChange = !didChange // force redraw
+    updateScale(newScale: scale * scaleMultiplier)
   }
 
+  /// Decreases the scale by the custom scale multiplier.
   func zoomOutCustom() {
-    scale /= scaleMultiplier
-    scale = scale.rounded() // Round to the nearest integer
-    doc.picdef.scale = scale
-    requiresFullCalc = true
-    didChange = !didChange // force redraw
+    updateScale(newScale: scale / scaleMultiplier)
   }
 
   var body: some View {
@@ -58,7 +68,7 @@ struct TabFindScale: View {
         Text("Scale (scale)")
         TextField("Scale", text: $scaleString, onCommit: {
           if let newScale = Double(scaleString) {
-            doc.picdef.scale = newScale.rounded()
+            updateScale(newScale: newScale)
           }
         })
         .textFieldStyle(.roundedBorder)
